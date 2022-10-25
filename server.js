2c660c1c12f4cc70fb86c5c04748fce18c02c904
mikias-worash <mikias.worash.a@gmail.com>
2022-10-25 11:10:21 +0300

    initial express setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const http = require('http')
+
+// Load env vars
+dotenv.config({ path: './config/config.env' })
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server is running at port ${PORT}`))
