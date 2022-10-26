472de62b7610431f4e3bd3eff0e07e822c509d64
mikias-worash <mikias.worash.a@gmail.com>
2022-10-26 15:22:37 +0300

    installed morgan

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const http = require('http')
-
+// load third party middleware (morgan) to display request methods, URL, etc
+const morgan = require('morgan')
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 
+const bootcamps = require('./routes/bootcamps')
+
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// Mount routes
+app.use('/', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server is running at port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(
+    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
+  )
+)
