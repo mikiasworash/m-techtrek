37094d5e9a8242b0259a34ca29b5dfe2cd0f6e54
mikias-worash <mikias.worash.a@gmail.com>
2022-10-31 22:31:25 +0300

    Added Components & Pages

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">E-Learning Platform</h1>
+      <p className="mb-4 text-2xl font-light">
+        A bootcamps directory where users can search and register for courses.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        Owner:
+        <a className="text-white" href="https://twitter.com/mikiasworash">
+          Mikias Worash
+        </a>
+      </p>
+    </>
+  )
+}
+
+export default About
