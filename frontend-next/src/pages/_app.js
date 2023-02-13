8845616f7c908e274525dfaeb7157d5b60d072f3
mikias-worash <mikias.worash.a@gmail.com>
2023-02-13 18:29:49 +0300

    tailwind configured

--- frontend-next/src/pages/_app.js
+++ frontend-next/src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <h1>nav</h1>
+      <Component {...pageProps} />
+      <h1>Footer</h1>
+    </>
+  )
 }
