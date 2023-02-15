cba21dbdbe09561757993cf410746324c17e574e
mikias-worash <mikias.worash.a@gmail.com>
2023-02-15 16:49:50 +0300

    added assets

---
+++ frontend-next/src/components/layout/Alert.js
@@ -0,0 +1,33 @@
+import { useContext } from 'react'
+import CourseContext from '@/context/CourseContext'
+
+function Alert() {
+  const { alertData } = useContext(CourseContext)
+
+  return (
+    alertData != null && (
+      <div className="flex items-start mb-4 space-x-2">
+        {alertData.type === 'error' && (
+          <svg
+            fill="none"
+            viewBox="0 0 24 24"
+            className="w-6 h-6 stroke-current mr-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              stroke="red"
+              d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+            ></path>
+          </svg>
+        )}
+        <p className="flex-1 text-base font-semibold leading-7 text-white">
+          <strong>{alertData.msg}</strong>
+        </p>
+      </div>
+    )
+  )
+}
+
+export default Alert
---
+++ frontend-next/src/components/layout/Spinner.js
@@ -0,0 +1,17 @@
+import spinner from '../../../public/img/spinner.gif'
+import Image from 'next/image'
+
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <Image
+        src={spinner}
+        alt="Loading..."
+        width={180}
+        className="text-center mx-auto text-black"
+      />
+    </div>
+  )
+}
+
+export default Spinner
