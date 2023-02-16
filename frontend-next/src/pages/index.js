04f3a07cb0abe8b30bdaf209de44ed1e4d926f20
mikias-worash <mikias.worash.a@gmail.com>
2023-02-16 16:26:03 +0300

    added featured courses

--- frontend-next/src/pages/index.js
+++ frontend-next/src/pages/index.js
@@ -4,8 +4,9 @@ import CourseResults from '@/components/courses/CourseResults'
 import CourseContext from '@/context/CourseContext'
 import { useContext } from 'react'
 import FeaturedCourses from '@/components/courses/FeaturedCourses'
+import Testimonials from '@/components/testimonials'
 
-export default function Home() {
+export default function Home({ featuredCourses }) {
   const { courses, coursesData } = useContext(CourseContext)
 
   return courses.length > 0 || coursesData.success === false ? (
@@ -14,7 +15,23 @@ export default function Home() {
     <>
       <Hero />
       <Features />
-      <FeaturedCourses />
+      <FeaturedCourses featuredCourses={featuredCourses} />
+      <Testimonials />
     </>
   )
+}
+
+export async function getStaticProps() {
+  const featuredTitle = 'dev'
+  const res = await fetch(
+    `http://localhost:5000/courses/title/${featuredTitle}`
+  )
+  const coursesData = await res.json()
+  const featuredCourses = coursesData.data
+
+  return {
+    props: {
+      featuredCourses,
+    },
+  }
 }
