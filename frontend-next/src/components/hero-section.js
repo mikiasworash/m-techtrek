e80a82b274350259612782df473d0dfc7691c195
mikias-worash <mikias.worash.a@gmail.com>
2023-02-17 15:54:13 +0300

    added hero image

--- frontend-next/src/components/hero-section.js
+++ frontend-next/src/components/hero-section.js
@@ -1,22 +1,24 @@
+import Image from 'next/image'
+import bootcampImage from '../../public/img/marvin-meyer-SYTO3xs06fU-unsplash.jpg'
 export default function Hero() {
   return (
     <>
       {/* Hero Section */}
       <section id="hero">
         {/* Flex container  */}
-        <div className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row">
+        <div className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 lg:flex-row">
           {/* Left Item  */}
           <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
-            <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
-              Unleash Your Potential with Our Bootcamp
+            <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl lg:text-left">
+              Unleash Your Potential with Our Platform
             </h1>
             <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
               Transform Your Career with Our Comprehensive Bootcamp Curriculum
               Designed to Help You Unlock Your Full Potential. Enroll Now!
             </p>
-            <div className="flex justify-center md:justify-start">
+            <div className="flex justify-center lg:justify-start">
               <a
-                href="#"
+                href="/register"
                 className="p-3 px-6 pt-2 text-white bg-purplish rounded-full baseline hover:bg-lightPurplish"
               >
                 Get Started
@@ -24,8 +26,13 @@ export default function Hero() {
             </div>
           </div>
           {/* Image for bootcamp  */}
-          <div className="md:w-1/2">
-            <img src="" alt="" />
+          <div className="hidden lg:flex flex-col lg:w-1/2 items-center">
+            <Image
+              src={bootcampImage}
+              alt="bootcamp img"
+              width={680}
+              className="rounded-full mb-24"
+            />
           </div>
         </div>
       </section>
